98cb9fb3a80532590be8d91af309326a4f0a3da1
Lubov Parfyonova <lubaparfyonova@gmail.com>
2022-10-26 14:05:11 +0300

    init reposite

---
+++ crud.session.sql
@@ -0,0 +1,8 @@
+CREATE TABLE things (
+    id serial PRIMARY KEY,
+    body text NOT NULL CHECK(body!=''),
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp NOT NULL DEFAULT current_timestamp
+);
+
+INSERT INTO things (body) VALUES ();
